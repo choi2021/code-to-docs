518ae9d84a1040609de98c53925533a9942748e9
Youngjun Choi <juni2004c@gmail.com>
2023-12-23 16:57:44 +0900

    feat: 일차적인 목표인 특정 유틸의 메소드를 읽고 param값을 markdown으로 정리할 수 있게 로직 구현

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,15 @@ interface SearchCriteria {
     methods?: MethodCriteria[];
 }
 
-const result: Record<string, number> = {};
+interface LogEntry {
+    title: string;
+    severity: string;
+    domain: string;
+}
 
+const logEntryMap = new Map<string, LogEntry>();
+
+const result: Record<string, number> = {};
 function readJSONFile(filePath: string): SearchCriteria[] {
     const jsonData = fs.readFileSync(filePath, 'utf8');
     return JSON.parse(jsonData).searchCriteria;
@@ -26,48 +33,69 @@ function analyzeProject(projectPath: string, criteria: SearchCriteria[]) {
     });
 
     criteria.forEach((criterion) => {
+        const hasProperties = criterion.properties && criterion.properties.length > 0;
+        const hasMethods = criterion.methods && criterion.methods.length > 0;
+
         project.getSourceFiles().forEach((sourceFile) => {
             sourceFile.forEachDescendant((node) => {
-                const isTargetNode = Node.isIdentifier(node) && node.getText() === criterion.name;
-                if (!isTargetNode) return;
-
                 // Check for object name usage
-                if (Node.isIdentifier(node) && node.getText() === criterion.name) {
-                    console.log(`Usage of ${criterion.name} found in file: ${sourceFile.getFilePath()}`);
-                    console.log(node.getText());
-                    result[criterion.name] = result[criterion.name] ? result[criterion.name] + 1 : 1;
+                if (!hasProperties && !hasMethods && Node.isIdentifier(node) && node.getText() === criterion.name) {
+                    return;
                 }
 
-                // Check for properties usage
-                if (Node.isPropertyAccessExpression(node) && node.getExpression().getText() === criterion.name) {
-                    const propertyName = node.getName();
-                    if (!criterion.properties || criterion.properties.includes(propertyName)) {
-                        console.log(`Property ${propertyName} found in file: ${sourceFile.getFilePath()}`);
-                        console.log(node.getText());
-                    }
-                    result[propertyName] = result[propertyName] ? result[propertyName] + 1 : 1;
-                }
+                // // Check for properties usage
+                // if (
+                //     hasProperties &&
+                //     Node.isPropertyAccessExpression(node) &&
+                //     node.getExpression().getText() === criterion.name
+                // ) {
+                //     const propertyName = node.getName();
+                //     if (criterion.properties?.includes(propertyName)) {
+                //         result[propertyName] = (result[propertyName] || 0) + 1;
+                //     }
+                // }
 
                 // Check for methods and paramProperties usage
-                if (
-                    Node.isCallExpression(node) &&
-                    node
-                        .getExpression()
-                        .getText()
-                        .startsWith(criterion.name + '.')
-                ) {
-                    const methodName = node.getExpression().getText().split('.')[1];
-                    const method = criterion.methods?.find((m) => m.methodName === methodName);
-                    if (method) {
-                        console.log(`Method ${methodName} found in file: ${sourceFile.getFilePath()}`);
-                        console.log(node.getText());
-                        const args = node.getArguments();
-                        args.forEach((arg, index) => {
-                            if (method.paramProperties && method.paramProperties[index]) {
-                                console.log(`Parameter ${method.paramProperties[index]}: ${arg.getText()}`);
-                                result[methodName] = result[methodName] ? result[methodName] + 1 : 1;
-                            }
-                        });
+                if (hasMethods && Node.isCallExpression(node)) {
+                    const callExpressionText = node.getExpression().getText();
+                    if (callExpressionText.startsWith(criterion.name + '.')) {
+                        const methodName = callExpressionText.split('.')[1];
+                        const targetMethod = criterion.methods?.find((m) => m.methodName === methodName);
+                        const targetMethodParams = targetMethod?.paramProperties ?? [];
+
+                        if (targetMethodParams.length === 0) {
+                            // If method has parameters, record the method name
+                            result[methodName] = (result[methodName] || 0) + 1;
+                        } else {
+                            const args = node.getArguments();
+                            const logEntry: LogEntry = {
+                                title: 'MapToError',
+                                severity: methodName,
+                                domain: '',
+                            };
+                            args.forEach((arg) => {
+                                if (Node.isObjectLiteralExpression(arg)) {
+                                    arg.forEachChild((node) => {
+                                        const chlid = node.getText();
+                                        const errorRegex = /new Error\('([^']*)'\)/;
+                                        const errorRegexMatches = chlid.match(errorRegex);
+
+                                        const tagRex = /tag: ErrorTag\.([^,]*)/;
+                                        const tagRegexMatches = chlid.match(tagRex);
+
+                                        if (errorRegexMatches) {
+                                            logEntry.title = errorRegexMatches[1];
+                                        }
+                                        if (tagRegexMatches) {
+                                            logEntry.domain = tagRegexMatches[1];
+                                        }
+                                    });
+                                }
+                            });
+
+                            const key = `${logEntry.title}-${logEntry.severity}-${logEntry.domain}`;
+                            logEntryMap.set(key, logEntry);
+                        }
                     }
                 }
             });
@@ -75,12 +103,22 @@ function analyzeProject(projectPath: string, criteria: SearchCriteria[]) {
     });
 }
 
+function generateMarkdownTable(entries: typeof logEntryMap): string {
+    let markdownTable = `| Title | Severity | Domain |\n`;
+    markdownTable += `|-------|----------|--------|\n`;
+
+    entries.forEach((entry) => {
+        markdownTable += `| ${entry.title} | ${entry.severity} | ${entry.domain} |\n`;
+    });
+
+    return markdownTable;
+}
+
 function index(jsonFilePath: string, projectPath: string) {
     const searchCriteria = readJSONFile(jsonFilePath);
     analyzeProject(projectPath, searchCriteria);
-    console.log('%c---------------------------', 'color:red');
-    console.log('result', result);
-    console.log('%c---------------------------', 'color:red');
+    const markdownTable = generateMarkdownTable(logEntryMap);
+    console.log(markdownTable);
 }
 
 const filePath = process.argv[2];
