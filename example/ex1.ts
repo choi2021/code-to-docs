e83f5b3d1f6fc0f1fc804d09aba11304ed40bb56
Youngjun Choi <juni2004c@gmail.com>
2023-12-23 16:57:57 +0900

    setting: 예시 수정

--- example/ex1.ts
+++ example/ex1.ts
@@ -1,18 +1,59 @@
-
-const CrashAnalyticsService={
-    sendInfo: ({error,originalError,tag}:{error:Error,originalError?:unknown,tag:keyof typeof ErrorTag}) => {
-        console.log(error,originalError,tag);
-    }
+const CrashAnalyticsService = {
+    info: 1,
+    sendInfo: ({
+        error,
+        originalError,
+        tag,
+    }: {
+        error: Error;
+        originalError?: unknown;
+        tag: keyof typeof ErrorTag;
+    }) => {
+        console.log(error, originalError, tag);
+    },
+    sendWarning: ({
+        error,
+        originalError,
+        tag,
+    }: {
+        error: Error;
+        originalError?: unknown;
+        tag: keyof typeof ErrorTag;
+    }) => {
+        console.log(error, originalError, tag);
+    },
+    sendError: ({
+        error,
+        originalError,
+        tag,
+    }: {
+        error: Error;
+        originalError?: unknown;
+        tag: keyof typeof ErrorTag;
+    }) => {
+        console.log(error, originalError, tag);
+    },
 };
 
-
-const ErrorTag={
-    test1:'test1',
+const ErrorTag = {
+    test1: 'test1',
 } as const;
 
-const initTask =async () => {
+const initTask = async () => {
     CrashAnalyticsService.sendInfo({
-            error: new Error('고객 잔여 캐시 조회 실패'),
-            tag: ErrorTag.test1,
-        });
+        error: new Error('info'),
+        tag: ErrorTag.test1,
+    });
+
+    CrashAnalyticsService.sendWarning({
+        error: new Error('warning'),
+        tag: ErrorTag.test1,
+    });
+
+    CrashAnalyticsService.sendError({
+        error: new Error('error'),
+        tag: ErrorTag.test1,
+    });
 };
+
+CrashAnalyticsService.info;
