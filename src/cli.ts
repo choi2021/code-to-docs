7116ba805e9e34d3e2d43653bffb5ce79c6c1fc3
Youngjun Choi <juni2004c@gmail.com>
2024-01-10 01:24:41 +0900

    feat: package 버전과 동기화

--- src/cli.ts
+++ src/cli.ts
@@ -1,22 +1,21 @@
 #!/usr/bin/env node
 
-import * as figlet from "figlet";
-import {Command} from "commander";
-import {main} from "./index";
+import * as figlet from 'figlet';
+import { Command } from 'commander';
+import { main } from './index';
+import pkg from '../../package.json';
 
 const program = new Command();
 
-console.log(figlet.textSync("Code To DOCS"));
+console.log(figlet.textSync('Code To DOCS'));
 
-program
-    .version('0.0.1')
-    .action(()=>{
-        const projectPath = process.argv[2];
-        const markdownOutputPath = process.argv[3] || 'analysisResults.md';
+program.version(pkg.version).action(() => {
+    const projectPath = process.argv[2];
+    const markdownOutputPath = process.argv[3] || 'analysisResults.md';
 
-        console.log(`projectPath: ${projectPath}`);
-        main(projectPath,markdownOutputPath);
-        console.log(`변환을 완료했습니다. ${markdownOutputPath} 파일을 확인해주세요.`);
-    });
+    console.log(`projectPath: ${projectPath}`);
+    main(projectPath, markdownOutputPath);
+    console.log(`변환을 완료했습니다. ${markdownOutputPath} 파일을 확인해주세요.`);
+});
 
 program.parse(process.argv);
